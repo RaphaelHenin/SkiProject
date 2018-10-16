8c5f489a7a69ad054818bb1daa3adc1c6db57e97
HENIN Raphael <raphael.henin2@soprasteria.com>
2018-10-16 12:10:19 +0200

    Editing the first version of the project

--- Ski project/src/Arc.java
+++ Ski project/src/Arc.java
@@ -4,65 +4,14 @@ public class Arc {
 	Node source;
 	Double time;
 	String niveauPisteOuModeTransport;
-
-	public Arc(String name, String niveauPisteOuModeTransport, Node source, Node destination) {
+	
+	public Arc(String name, String niveauPisteOuModeTransport,Node source, Node destination, Double time) {
 		super();
 		this.name = name;
 		this.destination = destination;
 		this.source = source;
+		this.time = time;
 		this.niveauPisteOuModeTransport = niveauPisteOuModeTransport;
-		this.time = calculTime();	
-	}
-
-	private Double calculTime() {
-		Double time;
-		int diffAltitudePiste =source.getAltitude() - destination.getAltitude();
-		int diffAltitudeRemonterMeca = -diffAltitudePiste;
-		switch (niveauPisteOuModeTransport) {
-		case "V":
-			time = 0.05 * (diffAltitudePiste);
-			break;
-		case "B":
-			time = 0.04 * (diffAltitudePiste);
-			break;
-		case "R":
-			time = 0.03 * (diffAltitudePiste);
-			break;
-		case "N":
-			time = 0.02 * (diffAltitudePiste);
-			break;
-		case "KL":
-			time = (10.0 / 60) * (diffAltitudePiste);
-			break;
-		case "SURF":
-			time = 0.1 * (diffAltitudePiste);
-			break;
-		case "TPH":
-			time = 4 + 0.02 * (diffAltitudeRemonterMeca);
-			break;
-		case "TC":
-			time = 2 + 0.03 * (diffAltitudeRemonterMeca);
-			break;
-		case "TSD":
-			time = 1 + 0.03 * (diffAltitudeRemonterMeca);
-			break;
-		case "TS":
-			time = 1 + 0.04 * (diffAltitudeRemonterMeca);
-			break;
-		case "TK":
-			time = 1 + 0.04 * (diffAltitudeRemonterMeca);
-			break;
-		case "BUS":
-			if(name.equals("navette1600-1800") || name.equals("navette1800-1600"))
-				time = 30.0;			
-			else
-				time = 40.0;
-			break;
-		default:
-			time=null;
-			break;
-		}
-		return time;
 	}
 
 	public String getName() {
@@ -88,7 +37,7 @@ public class Arc {
 	public void setTime(Double time) {
 		this.time = time;
 	}
-
+	
 	public Node getSource() {
 		return source;
 	}
--- Ski project/src/Main.java
+++ Ski project/src/Main.java
@@ -1,18 +1,18 @@
+import java.awt.List;
 import java.io.FileNotFoundException;
-import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 
 public class Main {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		
 		// Store the workspace path
 		String workspacePath = System.getProperty("user.dir");
-		
 		// Store nodes from the CSV "nodes.csv" to an ArrayList (ReadCSVFile function
-		// return an arraylist in which, each item is a line of the csv).
-		ArrayList<String[]> CSVnodes = TestReadCSV.ReadCSVFile(workspacePath + "\\src\\nodes.csv");
+		// return an arraylist
+		// in which, each item is a line of the csv).
+		ArrayList<String[]> CSVnodes = TestReadCSV.ReadCSVFile(workspacePath + "\\src\\vertices.csv");
 
 		// Declare an ArrayList to store the nodes read in the csv file
 		ArrayList<Node> nodes = new ArrayList<Node>();
@@ -24,54 +24,37 @@ public class Main {
 		// Altitude correspond to the third column of the CSV
 		// We need to parse the ID and Altitude to Integer because the CSV return String
 		for (int i = 0; i < CSVnodes.size(); i++) {
-			nodes.add(new Node(Integer.valueOf(CSVnodes.get(i)[0]), CSVnodes.get(i)[1],
-					Integer.valueOf(CSVnodes.get(i)[2])));
+			nodes.add(new Node(Integer.valueOf(CSVnodes.get(i)[0]), CSVnodes.get(i)[1], Integer.valueOf(CSVnodes.get(i)[2])));
 		}
 
 		// Store relations between each nodes from the CSV "data.csv" to an ArrayList
-		ArrayList<String[]> CSVarc = TestReadCSV.ReadCSVFile(workspacePath + "\\src\\data.csv");
-		
-		// Declare an ArrayList to store the edges read in the csv file
+		ArrayList<String[]> CSVarc = TestReadCSV.ReadCSVFile(workspacePath + "\\src\\relation_vertices.csv");
 		ArrayList<Arc> arcs = new ArrayList<Arc>();
-		
-		//Populating the arcs ArrayList with the values of the CSV file (CSVarc ArrayList).
+		// For each nodes in the ArrayList CSVarc, we associate to them the nodes which it
+		// is in relation with.
+		// Each node has a Map of his "adjacent" node.
+		// We associate them by calling the Node.addDestination function which takes in
+		// argument :
+		// The associated node (Third column of the csv)
+		// The time in minutes to go to the source node to the associated node.
+		// The travel time, depend on the type (V,B,R,N, etc..) -- TO DO --
 		for (int i = 0; i < CSVarc.size(); i++) {
-			arcs.add(new Arc(CSVarc.get(i)[0], CSVarc.get(i)[1], nodes.get(Integer.valueOf(CSVarc.get(i)[3]) - 1),
-					nodes.get(Integer.valueOf(CSVarc.get(i)[2]) - 1)));
+			arcs.add(new Arc(CSVarc.get(i)[0], CSVarc.get(i)[1],nodes.get(Integer.valueOf(CSVarc.get(i)[3])-1),nodes.get(Integer.valueOf(CSVarc.get(i)[2])-1), 1.0));//Integer.valueOf(destination.get(i)[6])));
 		}
-		
-		//writeInCSVFile.write(arcs, workspacePath+"\\src\\adjacentMatrix.csv");
-		//Create the graph
+
 		Graph graph = new Graph(nodes, arcs);
+
+
+		writeInCSVFile.write(arcs, workspacePath+"\\src\\adjacentMatrix.csv");
+
 		
-		//User IDs with his level
-		User user = new User("Raph", "Henin", "Débutant");
-		
-		Dijkstra dijkstra = new Dijkstra(graph, user);
-		
-		//Define the source node (Id node - 1)
-		dijkstra.execute(nodes.get(3));
-		
-		//Store the path from source to the destination node (Id node - 1)
-		LinkedList<Node> path = dijkstra.getPath(nodes.get(26));
-		
-		//Store the edges covered by Dijkstra
-		ArrayList<Arc> arcCoveredByPath = dijkstra.getArcsCovered();
-		
-		if (path == null) {
-			System.out.println("Path does not exist");
-		} else {
-			for (Node node : path) {
-				System.out.println(node.getId());
-			}
-			double time = 0.0;
-			DecimalFormat df = new DecimalFormat(".00");
-			for(Arc arc : arcCoveredByPath) {
-				System.out.println(arc.name + " "+ arc.niveauPisteOuModeTransport + " "+df.format(arc.getTime()));
-				time+=arc.getTime();
-			}
-			System.out.println("Total time = "+df.format(time)+ " minutes");
-		}
+		Dijkstra dijkstra = new Dijkstra(graph);
+        dijkstra.execute(nodes.get(0));
+        LinkedList<Node> path = dijkstra.getPath(nodes.get(12));
+
+        for (Node node : path) {
+            System.out.println(node.getId());
+        }
 
 	}
 }
