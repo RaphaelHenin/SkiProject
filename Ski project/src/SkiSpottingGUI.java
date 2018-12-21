1a2aa4db9092529d34f93cec3d4243d60f7d4258
Raphael Henin <heniin.raphael@gmail.com>
2018-12-21 16:16:43 +0100

    Design correction (console output) and main.class deleted

--- Ski project/src/SkiSpottingGUI.java
+++ Ski project/src/SkiSpottingGUI.java
@@ -6,16 +6,21 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+
 import com.jgoodies.forms.layout.FormLayout;
 import com.jgoodies.forms.layout.ColumnSpec;
 import com.jgoodies.forms.layout.RowSpec;
 import com.jgoodies.forms.layout.FormSpecs;
 import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import javax.swing.JTextArea;
+import java.awt.Color;
 
 public class SkiSpottingGUI {
 
@@ -56,13 +61,13 @@ public class SkiSpottingGUI {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 429);
+		frame.setBounds(100, 100, 825, 398);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new FormLayout(new ColumnSpec[] {
-				FormSpecs.RELATED_GAP_COLSPEC,
-				ColumnSpec.decode("default:grow"),
-				FormSpecs.RELATED_GAP_COLSPEC,
-				FormSpecs.DEFAULT_COLSPEC,},
+				ColumnSpec.decode("25px"),
+				ColumnSpec.decode("350px"),
+				ColumnSpec.decode("25px"),
+				ColumnSpec.decode("400px"),},
 			new RowSpec[] {
 				FormSpecs.RELATED_GAP_ROWSPEC,
 				FormSpecs.DEFAULT_ROWSPEC,
@@ -98,9 +103,18 @@ public class SkiSpottingGUI {
 		JLabel lblName = new JLabel("Name");
 		frame.getContentPane().add(lblName, "2, 2");
 		
+		JLabel lblResult = new JLabel("Result :");
+		frame.getContentPane().add(lblResult, "4, 2");
+		
 		textName = new JTextField();
 		frame.getContentPane().add(textName, "2, 4, left, default");
 		textName.setColumns(10);
+		
+		JTextArea textResult = new JTextArea();
+		textResult.setEditable(false);
+		JScrollPane scroll = new JScrollPane(textResult);
+		scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		frame.getContentPane().add(scroll, "4, 4, 1, 25, fill, fill");
 		
 		JLabel lblLastname = new JLabel("Lastname");
 		frame.getContentPane().add(lblLastname, "2, 6");
@@ -127,45 +141,6 @@ public class SkiSpottingGUI {
 		
 		JComboBox<String> To_list = new JComboBox<String>();
 		frame.getContentPane().add(To_list, "2, 20, fill, default");
-		
-		JButton btnGo = new JButton("Go !");
-		btnGo.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				//Create the graph
-				graph = new Graph(nodes, edges);
-				
-				//User IDs with his level
-				user = new User(textName.getText(), textLastName.getText(), Level_list.getSelectedItem().toString());
-				
-				dijkstra = new Dijkstra(graph, user);
-				
-				//Define the source node (Id node - 1)
-				dijkstra.execute(nodes.get(From_list.getSelectedIndex()));
-				
-				//Store the path from source to the destination node (Id node - 1)
-				LinkedList<Node> path = dijkstra.getPath(nodes.get(To_list.getSelectedIndex()));
-				
-				//Store the edges covered by Dijkstra
-				ArrayList<Edge> arcCoveredByPath = dijkstra.getArcsCovered();
-				
-				if (path == null) {
-					System.out.println("Path does not exist");
-				} else {
-					for (Node node : path) {
-						System.out.println(node.getId());
-					}
-					double time = 0.0;
-					DecimalFormat df = new DecimalFormat(".00");
-					for(Edge arc : arcCoveredByPath) {
-						System.out.println(arc.getName() + " "+ arc.getTransportType() + " "+df.format(arc.getTime()));
-						time+=arc.getTime();
-					}
-					System.out.println("Total time = "+df.format(time)+ " minutes");
-				}
-			}
-		});
-		frame.getContentPane().add(btnGo, "2, 30");
 		
 		// Store the workspace path
 		String workspacePath = System.getProperty("user.dir");
@@ -195,6 +170,56 @@ public class SkiSpottingGUI {
 		}
 		From_list.setModel(new DefaultComboBoxModel<String>(listOfNodes));
 		To_list.setModel(new DefaultComboBoxModel<String>(listOfNodes));
+		
+		JButton btnGo = new JButton("Go !");
+		btnGo.setBackground(Color.LIGHT_GRAY);
+		btnGo.setForeground(Color.BLACK);
+		btnGo.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				textResult.setText("Hello "+textName.getText()+" "+ textLastName.getText()+"\n"
+						+ "Here is the proposed itinerary to go from the summit "+From_list.getSelectedItem().toString()
+						+" to the summit "+To_list.getSelectedItem().toString()+" : \n");
+				//Create the graph
+				graph = new Graph(nodes, edges);
+				
+				//User IDs with his level
+				user = new User(textName.getText(), textLastName.getText(), Level_list.getSelectedItem().toString());
+				
+				dijkstra = new Dijkstra(graph, user);
+				
+				//Define the source node (Id node - 1)
+				dijkstra.execute(nodes.get(From_list.getSelectedIndex()));
+				
+				//Store the path from source to the destination node (Id node - 1)
+				LinkedList<Node> path = dijkstra.getPath(nodes.get(To_list.getSelectedIndex()));
+				
+				//Store the edges covered by Dijkstra
+				ArrayList<Edge> arcCoveredByPath = dijkstra.getArcsCovered();
+				
+				if (path == null) {
+					System.out.println("Path does not exist");
+					textResult.setText("Path does not exist \n");
+				} else {
+					textResult.setText(textResult.getText()+"List of summits through which you will pass : \n");
+					for (Node node : path) {
+						System.out.println(node.getId());
+						textResult.setText(textResult.getText()+node.getId().toString()+"\n");
+					}
+					double time = 0.0;
+					DecimalFormat df = new DecimalFormat(".00");
+					textResult.setText(textResult.getText()+"List of ski slopes you need to use (name, type or difficulty, approx. time) : \n");
+					for(Edge arc : arcCoveredByPath) {
+						System.out.println(arc.getName() + " "+ arc.getTransportType() + " "+df.format(arc.getTime()));
+						time+=arc.getTime();
+						textResult.setText(textResult.getText()+arc.getName() + " "+ arc.getTransportType() + " "+df.format(arc.getTime())+"\n");
+					}
+					System.out.println("Total time = "+df.format(time)+ " minutes");
+					textResult.setText(textResult.getText()+"Total time = "+df.format(time)+ " minutes \n");
+				}
+			}
+		});
+		frame.getContentPane().add(btnGo, "2, 26, 1, 3, fill, fill");
 		// Store relations between each nodes from the CSV "data.csv" to an ArrayList
 		ArrayList<String[]> csv_edge = ReadCSV.ReadCSVFile(workspacePath + "\\src\\relation_vertices.csv");
 		
